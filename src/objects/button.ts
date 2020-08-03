1e34fae99d6d76667fd1aa775e1dd722d6facfa3
S-Kaito <irc.darjeeling@gmail.com>
2020-08-03 19:52:09 +0900

    Fix button border

--- src/objects/button.ts
+++ src/objects/button.ts
@@ -15,8 +15,8 @@ export default class Button extends Phaser.GameObjects.Container {
 		super(scene, x, y)
 
 		const {
-			width = 80,
-			height = 120,
+			width = 200,
+			height = 40,
 			onClick
 		} = props
 
@@ -29,7 +29,8 @@ export default class Button extends Phaser.GameObjects.Container {
 		this.text = scene.add.text(alignLeft ? -width / 2 + 0 : 0, -1, text, { align, fontSize , color}).setOrigin(alignLeft ? 0 : 0.5, 0.5).setPadding(0, 2, 0, 0)
 		this.text.setColor("black");
 
-		this.container = scene.add.rectangle(20, 20, width / 2, height / 2);
+		this.container = scene.add.rectangle(0, 0, width / 2, height / 2);
+		this.container.setStrokeStyle(1, 0xffffff).setOrigin(alignLeft ? 0 : 0.5, 0.5)
 
 		this.add([this.container, this.text])
 		this.on('pointerover', () => {
